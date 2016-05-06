133314575c857c79f4346dd6e5c28a355a4fea49
Quentin LABORDE <lq202052@etu.unice.fr>
2016-05-06 11:17:47 +0200

    qeedzed

--- dotnet-app/requirement.sh
+++ dotnet-app/requirement.sh
@@ -1,3 +1,8 @@
-#sudo apt-get -y upgrade
-#sudo apt-get -y update
-sudo apt-get install -y mono-runtime
+#!/bin/bash
+
+apt-get update
+# apt-get download mono-runtime
+
+# sudo apt-get remove --auto-remove libmono-system-servicemodel-web4.0-cil
+apt-get install -y libmono-system-servicemodel-web4.0-cil
+apt-get install -y mono-runtime
